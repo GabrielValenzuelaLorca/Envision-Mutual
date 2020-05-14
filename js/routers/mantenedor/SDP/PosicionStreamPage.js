8834b4dbdad9a7ef38fb42007abd55e9d0e6abab
Johan Lizama <johanlizamadiaz@gmail.com>
2020-05-14 16:32:32 -0400

    add support to SDP request "Vacante"

--- js/routers/mantenedor/SDP/PosicionStreamPage.js
+++ js/routers/mantenedor/SDP/PosicionStreamPage.js
@@ -57,13 +57,71 @@ posicionStreamPage.methods.getMultiItemsSelectedButtons = function(items){
     return buttons;
 }
 
-posicionStreamPage.methods.getCamlQueryConditions = function(){
-    return `    
-                    
-                <Contains><FieldRef Name="Estado" /><Value Type="Choice">En revisión</Value></Contains>
-            `
-}
-
 posicionStreamPage.methods.getCamlOrderBy = function() {
     return '<FieldRef Name="NPosicion" Ascending="True" />';
+}
+
+posicionStreamPage.methods.renderHeader = function($header){
+    // core vars
+    var self = this,
+        page = this._getPage(),
+        url = page.route.url
+
+    // use vars
+    var title = self.getTitle() || page.route.query.title,
+        description = '',
+        tabChoices = [
+            {id : 1, text: 'Todas las posiciones', segment: 'Disponible para uso'},
+            {id : 2, text: 'En revisión', segment: 'En validación'},
+            {id : 3, text: 'En validacion de Jefatura', segment: 'Validación Jefe CyE'}
+        ];
+
+    // tempalte con titulo, descripción opcional y un tabs
+    var templateHtml = `
+        <div class="form-header">
+            <div class="Title" style="margin-left: 42%; margin-right: 42%; margin-top: 8px; font-size: 20px; font-weight: 600;">` + title +  `</div>
+            <div class="ms-Form-Description">` + description + `</div>
+            <div class="segmented" style="margin:20px auto; max-width:600px">
+                <a class="button" segment-id="` + tabChoices[0].id + `" segment-text="` + tabChoices[0].segment + `">` + tabChoices[0].text + `</a>
+                <a class="button" segment-id="` + tabChoices[1].id + `" segment-text="` + tabChoices[1].segment + `">` + tabChoices[1].text + `</a>
+                <a class="button" segment-id="` + tabChoices[2].id + `" segment-text="` + tabChoices[2].segment + `">` + tabChoices[2].text + `</a>
+            </div>
+        </div>
+    `;
+
+    // agregar html
+    $header.html(templateHtml);
+
+    // agregar al hacer click sobre botón
+    $header.find('.segmented .button').on('click', function(e){
+        var choice = this.getAttribute('segment-text');
+            query = "";
+        switch(choice){
+            case tabChoices[0].segment.toString() :{
+                query = `
+                        <Eq><FieldRef Name="Estado" /><Value Type="Choice">${tabChoices[0].segment.toString()}</Value></Eq>
+                    `;
+                break;
+            }
+            case tabChoices[1].segment.toString() :{
+                query = `
+                        <Eq><FieldRef Name="Estado" /><Value Type="Choice">${tabChoices[1].segment.toString()}</Value></Eq>
+                    `;
+                break;
+            }
+            case tabChoices[2].segment.toString() :{
+                query = `
+                        <Eq><FieldRef Name="Estado" /><Value Type="Choice">${tabChoices[2].segment.toString()}</Value></Eq>
+                    `;
+                break;
+            }
+        }
+        // manejar lógica para ver botón seleccionado
+        $header.find('.segmented .button').removeClass('button-active');
+        $(this).addClass('button-active');
+        
+
+        // self.requestItems() acepta como parametro una consulta CAML
+        self.requestItems(query);
+    });
 }
